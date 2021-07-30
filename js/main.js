02b6066686f5e4a1e6a641b5a21f178005adce50
Javier Herrera <javier.ivancouoh@gmail.com>
2021-07-30 16:08:48 -0500

    Add new item to the list

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 import Task from './Task.js';
 import TaskList from './TaskList.js';
 
-const tasksArray = [
-    /* Fake data */
-    // remove all items in array when finished program.
-    new Task(1, 'tarea 1', '20-07-2021'),
-    new Task(2, 'tarea 6', '17-07-2021'),
-    new Task(3, 'tarea 14', '08-05-2021'),
-    /* End fake data */
-];
+const taskList = new TaskList('task-list', []);
 
-const taskList = new TaskList('task-list', tasksArray);
+const addTask = () => {
+    let inputElement = document.getElementById('task-name');
+    let name = inputElement.value;
+
+    if (name !== '') {
+        let task = new Task(null, name, null);
+        taskList.add(task);
+        inputElement.value = "";
+        inputElement.placeholder = "Task Name";
+        inputElement.parentElement.classList.remove('validate');
+    } else {
+        inputElement.parentElement.classList.add('validate');
+        inputElement.placeholder = "Not empty";
+    }
+}
 
 /* Start function */
 const main = () => {
+    /* Events */
+    document.getElementById('add-task').addEventListener('click', () => {
+        addTask();
+    });
+
+    document.getElementById('task-name').addEventListener('keyup', () => {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
 }
 
 // Initialize script
