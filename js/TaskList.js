9329bdfd0984b12302c78695248912010df4b726
josue1919 <52299245+josue1919@users.noreply.github.com>
2021-08-04 11:07:04 -0500

    Edit task

--- js/TaskList.js
+++ js/TaskList.js
@@ -1,66 +1,81 @@
-import Task from './Task.js';
+import Task from "./Task.js";
 class TaskList {
-    constructor(element_id, items) {
-        this.element_id = element_id;
-        /* items<Task> */
-        this.items = items;
-        this.autoincrementId = 1;
+  constructor(element_id, items) {
+    this.element_id = element_id;
+    /* items<Task> */
+    this.items = items;
+    this.autoincrementId = 1;
 
-        this.update();
-    }
+    this.update();
+  }
 
-    update() {
-        let listElement = document.getElementById(this.element_id);
-        listElement.innerHTML = '';
-        let rawTextNodes = '';
+  update() {
+    let listElement = document.getElementById(this.element_id);
 
-        this.items.forEach(element => {
-            rawTextNodes += `
-            <div data-id="${element.id}">
-                ${element.name}
-                <small class="ml-3">
+    listElement.innerHTML = "";
+    let rawTextNodes = "";
+
+    this.items.forEach((element) => {
+      rawTextNodes += `
+            <div data-id="${element.id}" class="items">
+            <input type='text' class='input-invisible' id="${element.id}"></input>
+                <p class='nombre' id="${element.id}" >${element.name}</p>
+                <small class="ml-3 mr-3">
                     ${element.date}
                 </small>
-            </div>`;
-        });
+               
+                <button class="btn-edit" id="${element.id}"><i class="fas fa-edit"></i></button>
+                <button class="btn-save" id="${element.id}"><i class="fas fa-save"></i></button>
 
-        listElement.innerHTML = rawTextNodes;
-    }
+            </div>
+            `;
+    });
 
-    add(task) {
-        /* Metodo add se encarga unicamente de guardar datos */
-        let date = new Date();
-        let hour = ('0' + date.getHours()).slice(-2);
-        let minute = ('0' + date.getMinutes()).slice(-2);
-        let day = ('0' + date.getDate()).slice(-2);
-        let month = ('0' + (date.getMonth() + 1)).slice(-2);
-        let year = date.getFullYear();
+    listElement.innerHTML = rawTextNodes;
+  }
 
-        this.items.push(
-            new Task(
-                this.autoincrementId,
-                task.name,
-                task.date === null ? `${day}-${month}-${year}, ${hour}:${minute}` : task.date
-            )
-        );
+  //add recibe el nombre de la tarea como parametro
+  add(task) {
+    /* Metodo add se encarga unicamente de guardar datos */
+    let date = new Date();
+    let hour = ("0" + date.getHours()).slice(-2);
+    let minute = ("0" + date.getMinutes()).slice(-2);
+    let day = ("0" + date.getDate()).slice(-2);
+    let month = ("0" + (date.getMonth() + 1)).slice(-2);
+    let year = date.getFullYear();
 
-        this.autoincrementId++;
+    //agrega al array los datos del constructor
+    this.items.push(
+      new Task(
+        this.autoincrementId,
+        task.name,
+        task.date === null
+          ? `${day}-${month}-${year}, ${hour}:${minute}`
+          : task.date
+      )
+    );
 
-        this.update();
-    }
+    this.autoincrementId++;
 
-    edit(object_id) {
-        /* code */
+    this.update();
+  }
 
-        this.update();
-    }
+  edit(object_id, idBtn) {
+    /* code */
 
-    delete(object_id) {
-        /* code */
+    const indice = idBtn - 1;
+    // console.log('indice es '+indice)
+    const neName = object_id;
+    this.items[indice].name = neName;
+    console.log(this.items);
+    this.update();
+  }
 
-        this.update();
-    }
+  delete(object_id) {
+    /* code */
 
+    this.update();
+  }
 }
 
 export default TaskList;
--- js/main.js
+++ js/main.js
@@ -1,39 +1,79 @@
-import Task from './Task.js';
-import TaskList from './TaskList.js';
+import Task from "./Task.js";
+import TaskList from "./TaskList.js";
 
-const taskList = new TaskList('task-list', []);
+const taskList = new TaskList("task-list", []);
 
 const addTask = () => {
-    let inputElement = document.getElementById('task-name');
-    let element = inputElement.value;
+  let inputElement = document.getElementById("task-name"); //el input que guarda el nombre
+  let element = inputElement.value; //guarda el valor del input
 
-    if (element !== '') {
-        let name = element.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-        let task = new Task(null, name, null);
-        taskList.add(task);
-        inputElement.value = "";
-        inputElement.placeholder = "Task Name";
-        inputElement.parentElement.classList.remove('validate');
-    } else {
-        inputElement.parentElement.classList.add('validate');
-        inputElement.placeholder = "Not empty";
+  if (element !== "") {
+    let name = element.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let task = new Task(null, name, null);
+    taskList.add(task); //manda como parametro al metodo add el nombre de la tarea
+    inputElement.value = "";
+    inputElement.placeholder = "Task Name";
+    inputElement.parentElement.classList.remove("validate");
+  } else {
+    inputElement.parentElement.classList.add("validate");
+    inputElement.placeholder = "Not empty";
+  }
+};
+
+const Edit = (parametro) => {
+  // console.log("el parametro es " + parametro);
+  const Indice = parametro - 1;
+
+  const btnEdit = document.querySelectorAll(".btn-edit")[Indice]; //botn de edicion
+  const btnSave = document.querySelectorAll(".btn-save")[Indice]; //btnSave de guardado
+  const inputEdit = document.querySelectorAll(".input-invisible")[Indice]; //input de edicion
+  const task = document.querySelectorAll(".nombre")[Indice];
+
+  if (
+    btnEdit.id === btnSave.id &&
+    btnEdit.id === inputEdit.id &&
+    btnEdit.id === task.id
+  ) {
+    btnEdit.classList.toggle("input-invisible");
+    btnSave.classList.toggle("visible");
+    inputEdit.classList.toggle("visible");
+    task.classList.toggle("input-invisible");
+  }
+
+  btnSave.addEventListener("click", () => {
+    if (btnEdit.getElementsByClassName.display === "none") {
+      btnEdit.classList.toggle("input-invisible");
+      btnSave.classList.toggle("visible");
+      inputEdit.classList.toggle("visible");
+      task.classList.toggle("input-invisible");
     }
-}
+  });
+
+  if (inputEdit.value !== "") {
+    taskList.edit(inputEdit.value, btnSave.id);
+  }
+};
 
 /* Start function */
 const main = () => {
-    /* Events */
-    document.getElementById('add-task').addEventListener('click', () => {
-        addTask();
-    });
+  /* Events */
+  document.getElementById("add-task").addEventListener("click", () => {
+    addTask();
+  });
 
-    document.getElementById('task-name').addEventListener('keyup', () => {
-        if (event.key === 'Enter') {
-            addTask();
-        }
-    });
+  document.getElementById("task-name").addEventListener("keyup", () => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
 
-}
+  document.getElementById("task-list").addEventListener("click", (e) => {
+    var btnSave = e.target.parentNode;
+    // ...
+    
+    Edit(btnSave.id);
+  });
+};
 
 // Initialize script
 main();
